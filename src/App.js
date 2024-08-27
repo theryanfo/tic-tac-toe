50ba00b277c7afafa1dcacf6234a6eb520b54cc0
theryanfo <ryan.s.fordham@gmail.com>
2024-08-26 21:41:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import { useState } from "react";
 
-export default function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1]
 
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
     function handleClick(i) {
       if (calculateWinner(squares) || squares[i]) {
         return
@@ -12,10 +49,9 @@ export default function Board() {
       if (xIsNext) {
         nextSquares[i] = "X";
       } else {
-        nextSquares[i] = "O"
+        nextSquares[i] = "O";
       }
-      setXIsNext(!xIsNext);
-      setSquares(nextSquares);
+      onPlay(nextSquares);
     }
 
     const winner = calculateWinner(squares);
@@ -23,7 +59,7 @@ export default function Board() {
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
     }
 
     return (
